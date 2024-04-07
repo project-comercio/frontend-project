770796bd26110cc7c0da8a120dc224c691df6e05
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-04-07 16:26:12 -0300

    removed gmt from lib

--- lib/gmt.ts
+++ lib/gmt.ts
@@ -1,8 +1,8 @@
-export const GTM_ID = 'GTM-NHP435N'
+// export const GTM_ID = 'GTM-NHP435N'
 
-export const pageview = (url: string) => {
-  window?.dataLayer?.push({
-    event: 'pageview',
-    page: url
-  })
-}
+// export const pageview = (url: string) => {
+//   window?.dataLayer?.push({
+//     event: 'pageview',
+//     page: url
+//   })
+// }
