663b072a0514b9ab26b5b2e2e7aa9fed314570ca
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-04-07 13:33:23 -0300

    Update getMetadata function

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -2,16 +2,19 @@ import Navbar from '@/components/Navbar/Navbar'
 import React from 'react'
 import Feed from './sections/Feed'
 import { getUser } from '@/context/UserContext'
-import HeadComponent from '@/components/HeadComponent/HeadComponent'
+import { getMetaData } from '@/utils/getMetaData'
+
+export async function generateMetadata() {
+  return getMetaData({
+    title: "Feed de Postagens",
+    description: "Descrição do feed de postagens",
+    image: ""
+  })
+}
 
 const page = () => {
   return (
     <>
-      <HeadComponent
-        title='Página Inicial'
-        description='Veja com detalhes tudo que a nossa empresa pode fornecer para você! Garanto que seus resultados e sua visilidade irão crescer exponencialmente!'
-        opengraph=''
-      />
       <Navbar variant='primary' />
       <main className="lg:px-8 pb-8 px-4 min-h-[62vh] pt-28 bg-[#fafafa] flex flex-col items-center">
         <Feed />
--- components/HeadComponent/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface HeadComponentProps {
-  title: string
-  description: string
-  keywords?: string
-  opengraph?: string
-}
