d332eb878e7b2382b377e5e00bc112c02816752c
murilo <murilorockstar492@gmail.com>
2024-04-12 19:12:02 -0300

    alteraçao na area sobre, adicionado colaboradores

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar/Navbar'
 import { getMetaData } from '@/utils/getMetaData'
 import React from 'react'
 import Header from './sections/Header'
+import Collaborators from "./sections/Collaborators";
 
 export async function generateMetadata() {
   return getMetaData({
@@ -17,6 +18,7 @@ const page = () => {
       <Navbar variant="secondary" />
       <main className="min-h-[62vh] pt-16">
         <Header />
+        <Collaborators />
       </main>
     </>
   )
