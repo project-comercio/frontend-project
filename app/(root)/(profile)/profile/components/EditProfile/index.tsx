057c5ed267ae72d00f2e9e8d4d7997249d8929c3
unknown <vitorgabrielsbo1460@gmail.com>
2024-04-15 22:20:44 -0300

    create initial config for user update modal

--- app/(root)/(profile)/profile/components/EditProfile/index.tsx
+++
@@ -1,9 +0,0 @@
-import Popup from "@/components/Popups/BasePopup";
-
-export function EditProfile() {
-  return (
-    <Popup setShowState={setShowState} showState={showState} title="Editar Perfil" description="Personalize seu perfil e deixe ele atrativo para outros usuários">
-
-    </Popup>
-  )
-}
