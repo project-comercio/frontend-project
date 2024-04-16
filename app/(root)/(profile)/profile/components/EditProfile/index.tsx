5ef6dbd28bd38377bc5672ac7871531713266132
unknown <vitorgabrielsbo1460@gmail.com>
2024-04-15 21:41:02 -0300

    update clerk styles

---
+++ app/(root)/(profile)/profile/components/EditProfile/index.tsx
@@ -0,0 +1,9 @@
+import Popup from "@/components/Popups/BasePopup";
+
+export function EditProfile() {
+  return (
+    <Popup setShowState={setShowState} showState={showState} title="Editar Perfil" description="Personalize seu perfil e deixe ele atrativo para outros usuários">
+
+    </Popup>
+  )
+}
